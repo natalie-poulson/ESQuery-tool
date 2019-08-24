06c4239ab5b0eeac6db0c78d8bbe09bc8860126a
natalie-poulson <natalieplsn@gmail.com>
2019-08-24 15:43:55 -0700

    added basic dark mode css and js

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,31 @@ const inputNode = document.getElementById('input');
 const queryNode = document.getElementById('query');
 const outputNode = document.getElementById('output');
 const numOfNodes = document.getElementById('numOfNodes');
+// eslint-disable-next-line max-len
+const toggleSwitch = document.querySelector('.theme-switch-wrapper input[type="checkbox"]');
+
+// eslint-disable-next-line require-jsdoc
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+  }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+// eslint-disable-next-line max-len
+const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+  }
+}
 
 const update = () => {
   try {
