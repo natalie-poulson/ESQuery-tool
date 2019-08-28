0a604be5799ff643f32d907a00e7905babf88984
natalie-poulson <natalieplsn@gmail.com>
2019-08-27 20:13:28 -0700

    changed toggle button TODO fix local storage

--- scripts/index.js
+++ scripts/index.js
@@ -4,20 +4,17 @@ const outputNode = document.getElementById('output');
 const numOfNodes = document.getElementById('numOfNodes');
 const themeWrapper = document.querySelector('.theme-switch-wrapper');
 const textAreas = document.getElementsByClassName('theme-switch');
-const toggleBody = document.querySelector('.toggle-body');
-const toggleBtn = document.querySelector('.toggle-btn');
+const toggleBtn = document.querySelector('.switch');
 // eslint-disable-next-line max-len
 const savedTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 // fix this
 let light = true;
 
-toggleBtn.addEventListener('click', () => {
+toggleBtn.addEventListener('change', () => {
   themeWrapper.classList.toggle('theme-switch-wrapper--on');
   for (textArea of textAreas) {
     textArea.classList.toggle('theme-switch--on');
   }
-  toggleBody.classList.toggle('toggle-body--on');
-  toggleBtn.classList.toggle('toggle-btn--on');
 
   if (light) {
     localStorage.setItem('theme', 'dark');
@@ -35,8 +32,6 @@ if (savedTheme) {
     for (textArea of textAreas) {
       textArea.classList.add('theme-switch');
     }
-    toggleBody.classList.add('toggle-body');
-    toggleBtn.classList.add('toggle-btn');
   }
 }
 if (savedTheme === 'dark') {
@@ -44,8 +39,6 @@ if (savedTheme === 'dark') {
   for (textArea of textAreas) {
     textArea.classList.add('theme-switch--on');
   }
-  toggleBody.classList.add('toggle-body--on');
-  toggleBtn.classList.add('toggle-btn--on');
 }
 
 const update = () => {
